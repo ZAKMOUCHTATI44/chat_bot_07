1da449ce83693451045ae8c6c2875495bae887d8
Zakaria MOUCHTATI <z.mouchtati@gmail.com>
2025-07-10 14:03:25 +0100

    deploy

--- server.ts
+++ server.ts
@@ -205,7 +205,7 @@ app.post("/uir-chat-bot", async (req: Request, res: Response) => {
         question: message.Body,
       },
       {
-        configurable: { sessionId: message.From },
+        configurable: { sessionId: `${message.From}-1ihdaodbad` },
       }
     );
 
